1a6dd9c30103c75a84197ff2a0111575ffbba38e
Arnout Plettinx <arnoutplettinx@hotmail.com>
2024-12-25 16:18:40 +0100

    Endpoint login toegevoegd

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt'); // wachtwoordbeveiliging
 const Database = require('./classes/database.js');
 const User = require('./classes/user.js');
 const app = express();
+const JWT_SECRET = 'CampEasyPass_WebFundies_20242025_1613'
 
 
 app.use(express.json());
@@ -45,6 +46,41 @@ app.post('/api/users', async (req,res) => {
         [user.firstName, user.lastName, user.preferredName, user.roleUser, user.email, user.phoneNumber, user.address, user.country, user.password, user.emergencyTel, user.creationDate])
         .then(() => res.status(201).send({ message: 'User added successfully' }))
         .catch((error) => res.status(500).send({ error: 'Failed to add user', details: error }));
+})
+
+app.post('/api/login', async (req, res) => {
+    const {email, password} = req.body;
+    const db = new Database();
+
+    try {
+        const result = await db.getQuery('SELECT * FROM users WHERE email = ?', [email]);
+        if (result.length === 0) {
+            return res.status(401).json({ error: 'Foutieve ingave' });
+        }
+        const u = result[0];
+        const validPassword = await bcrypt.compare(password, u.password);
+
+        if (!validPassword){
+            return res.status(401).json({ error: 'Foutieve ingave' });
+        }
+
+        const token = jst.sign(
+            {userId: u.id, role: u.roleUser},
+            JWT_SECRET,
+            {expiresIn: '24h '}
+        )
+
+        res.json({
+            token,
+            userID: u.id,
+            userRole: u.role,
+            firstName: u.firstName,
+            prefName: u.prefName,
+            userMail: u.mail,
+        })
+    } catch (error){
+        res.status(500).json({error: 'Login failed'});
+    }
 })
 
 app.listen(3100, () => {
