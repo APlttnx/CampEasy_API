c84b1c16ea84852a385cfe42c6ae9943912e7d69
Arnout Plettinx <arnoutplettinx@hotmail.com>
2025-01-10 11:43:53 +0100

    Images! (wel maar 1 afbeelding)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt'); // wachtwoordbeveiliging
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+
 // const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_SECRET = 'CampEasyPass_WebFundies_20242025_1613'; //Zit normaal gezien in .env file, maar voor schoolopdracht even hier gezet
 // klasses importeren
@@ -12,15 +13,25 @@ const User = require('./classes/user.js');
 const Camping = require('./classes/camping.js');
 const app = express();
 
-
-
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:8080',
+    origin: 'http://localhost:8080',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type','Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
-app.set('maxHeaderSize', 16 * 1024 * 1024);  // Example: increase to 16 KB
+
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        cb(null, 'camping-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+app.use('/uploads', express.static('uploads'));
+
+app.set('maxHeaderSize', 16 * 1024 * 1024);
 
 
 app.get('/', (req, res) => {
@@ -29,9 +40,9 @@ app.get('/', (req, res) => {
 
 // ____________________________________________________________________________________________________________________________________
 //Users
-app.post('/api/users', async (req,res) => {
-    try{
-        const {firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel, password} = req.body;
+app.post('/api/users', async (req, res) => {
+    try {
+        const { firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel, password } = req.body;
         const user = new User(firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel, password, "", "", "");
 
         if (!user.isValidEmail()) {
@@ -44,33 +55,33 @@ app.post('/api/users', async (req,res) => {
             [user.firstName, user.lastName, user.preferredName, user.roleUser, user.email, user.phoneNumber, user.address, user.country, user.emergencyTel]
         );
 
-        if (result.insertId){
+        if (result.insertId) {
             user.id = result.insertId;
-        }else{
+        } else {
             const idResult = await DataView.getQuery('SELECT LAST_INSERT_ID() as id');
             user.id = idResult[0].id;
         }
 
         await db.getQuery('INSERT INTO passwords (userID, password) VALUES (?,? )',
-            [ user.id, user.password]
+            [user.id, user.password]
         );
 
         res.status(201).send({ message: 'User added successfully' });
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'Failed to add user', details: error })
     }
 })
 app.post('/api/login', async (req, res) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
         const db = new Database();
 
         const result = await db.getQuery(
             `SELECT u.id, u.roleUser, u.firstName, u.preferredName, p.password
             FROM users u
             JOIN passwords p ON u.id = p.userID
-            WHERE u.email = ?;`, 
+            WHERE u.email = ?;`,
             [email]);
         if (result.length === 0) {
             return res.status(401).json({ error: 'Foutieve ingave' });
@@ -78,30 +89,30 @@ app.post('/api/login', async (req, res) => {
         const u = result[0];
         const validPassword = await bcrypt.compare(password, u.password);
 
-        if (!validPassword){
+        if (!validPassword) {
             return res.status(401).json({ error: 'Foutieve ingave' });
         }
 
         const token = jwt.sign(
-            {userId: u.id, role: u.roleUser},
+            { userId: u.id, role: u.roleUser },
             JWT_SECRET,
-            {expiresIn: '24h'}
+            { expiresIn: '24h' }
         )
 
         res.json({
             token,
             userRole: u.roleUser,
             userGreetName: u.preferredName || u.firstName,
-            
+
         },
         );
-    } catch (error){
+    } catch (error) {
         res.status(500).send({ error: 'Login failed', details: error.message });
     }
 })
 app.get('/api/users', async (req, res) => {
-    try{
-        const token = req.headers['authorization'].replace('Bearer ','');
+    try {
+        const token = req.headers['authorization'].replace('Bearer ', '');
         const decoded = jwt.verify(token, JWT_SECRET);
         const userIdToken = decoded.userId
 
@@ -112,7 +123,7 @@ app.get('/api/users', async (req, res) => {
             return res.status(404).json({ error: 'User not found' });
         }
         const u = result[0];
-        const user = new User(u.firstName, u.lastName, u.preferredName, u.roleUser, u.email, u.phoneNumber, u.address, u.country,u.emergencyTel,"",u.creationDate,u.updateDate, u.id);
+        const user = new User(u.firstName, u.lastName, u.preferredName, u.roleUser, u.email, u.phoneNumber, u.address, u.country, u.emergencyTel, "", u.creationDate, u.updateDate, u.id);
 
         res.json({
             firstName: user.firstName,
@@ -127,21 +138,21 @@ app.get('/api/users', async (req, res) => {
             updateDate: user.updateDate,
         });
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
-app.put('/api/users', async(req,res) => {
-    try{
-        const token = req.headers['authorization'].replace('Bearer ','');
+app.put('/api/users', async (req, res) => {
+    try {
+        const token = req.headers['authorization'].replace('Bearer ', '');
         const decoded = jwt.verify(token, JWT_SECRET);
         const userIdToken = decoded.userId
 
         // console.log(userIdToken);
         // console.log(req.body);
 
-        const {firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel} = req.body;
-        const user = new User(firstName, lastName, preferredName, roleUser, email , phoneNumber, address, country, emergencyTel, "", "", "", userIdToken);
+        const { firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel } = req.body;
+        const user = new User(firstName, lastName, preferredName, roleUser, email, phoneNumber, address, country, emergencyTel, "", "", "", userIdToken);
         // console.log(user);
 
         const db = new Database();
@@ -154,53 +165,70 @@ app.put('/api/users', async(req,res) => {
         );
 
         res.status(201).send({ message: 'User updated successfully' });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send({ error: 'Failed to update user', details: error })
     }
 })
 //Campings
-app.post('/api/camping', async (req,res) => {
-    const token = req.headers['authorization'].replace('Bearer ','');
+app.post('/api/campingImageUpload', upload.single('image'), async (req, res) => {
+    try {
+        if (!req.file) return res.status(400).send("No image detected")
+        const imageUrl = `/uploads/${req.file.filename}`;
+        res.json({ imageUrl });
+    } catch (error) {
+        res.status(500).send({ error: "Failed to upload image" })
+    }
+})
+app.post('/api/camping', async (req, res) => {
+    const token = req.headers['authorization'].replace('Bearer ', '');
     const decoded = jwt.verify(token, JWT_SECRET);
     const userIdToken = decoded.userId
 
-    const {name, type, size, price, description, address, country, images,facilities, userRole, } = req.body;
+    const { name, type, size, price, description, address, country, imageUrl, facilities, userRole, } = req.body;
     //console.log(req.body);
     const camping = new Camping(name, type, size, price, description, address, country, userIdToken, "", "", "");
 
-    try{
+    try {
         const db = new Database();
         const result = await db.getQuery('INSERT INTO campings (name, type, size, price, description, address, country, ownerID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
             [camping.name, camping.type, camping.size, camping.price, camping.description, camping.address, camping.country, camping.ownerId]
         );
-        if (result.insertId){
+        if (result.insertId) {
             camping.id = result.insertId;
-        }else{
+        } else {
             const idResult = await DataView.getQuery('SELECT LAST_INSERT_ID() as id');
             camping.id = idResult[0].id;
         }
-        //afbeeldingen camping in aparte tabel
-        for (const picture of images) {
-            await db.getQuery(
-                'INSERT INTO campingpictures (campingID, picture) VALUES (?, ?)',
-                [camping.id, picture]
-            );
-        }
-        //rol aanpassen van creator als hij nog een 'user' was
-        if(userRole === "user"){
+        
+        //image upload
+        await db.getQuery(
+            'INSERT INTO campingpictures (campingID, picture) VALUES (?, ?)',
+            [camping.id, imageUrl]
+        );
+        //#region ongebruikte code voor meerdere image uploads
+        // for (const pictureUrl of imageUrl) {
+        //     await db.getQuery(
+        //         'INSERT INTO campingpictures (campingID, picture) VALUES (?, ?)',
+        //         [camping.id, pictureUrl]
+        //     );
+        // }
+        //#endregion
+
+
+        if (userRole === "user") {
             await db.getQuery(
                 `UPDATE users SET roleUser = ? WHERE id = ?`,
                 ['owner', camping.ownerId]
             );
             console.log("changed role to owner")
         }
-        
+
 
         //facilities aanvullen
         console.log('faciliteiten: ');
         //console.log(facilities);
-        for (const facilityID of facilities){
+        for (const facilityID of facilities) {
             await db.getQuery(
                 'INSERT INTO campingfacilities (campingID, facilityID) VALUES (?, ?)',
                 [camping.id, facilityID]
@@ -210,20 +238,20 @@ app.post('/api/camping', async (req,res) => {
         console.log(`camping ${camping.name} added successfully`)
         res.status(201).send({ message: 'Camping added successfully' });
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'Failed to add camping', details: error })
     }
 })
-app.get('/api/campingGeneralData', async (req,res) => {
+app.get('/api/campingGeneralData', async (req, res) => {
     const db = new Database();
-    try{
+    try {
         console.log("fetching campings");
         const result = await db.getQuery(`
             SELECT 
                 c.*,
                 u.firstName, u.lastName,
                 JSON_ARRAYAGG(f.facilityName) as facilities,
-                JSON_ARRAYAGG(i.picture) as imageUrls
+                MAX(i.picture) as imageUrl
             FROM campings c
             LEFT JOIN campingpictures i ON c.ID = i.campingID
             LEFT JOIN campingfacilities cf ON c.ID = cf.campingID
@@ -231,28 +259,33 @@ app.get('/api/campingGeneralData', async (req,res) => {
             JOIN users u ON c.OwnerID = u.ID 
             GROUP BY c.ID;
         `)
-    //    console.log(result);
+        //    console.log(result);
         const processedResult = result.map(camping => {
             const formattedDate = camping.updateDate
                 ? new Date(camping.updateDate).toLocaleDateString('en-GB')
                 : null;
-           
+
+            //voor development
+            const adaptedImageUrl = !!camping.imageUrl
+                ? `http://localhost:3100${camping.imageUrl}#/`
+                : '';
+            console.log(adaptedImageUrl);
             return {
                 ...camping,
-                image: '',
+                imageUrl: adaptedImageUrl,
                 updateDate: formattedDate,
             };
-        });       
+        });
 
         res.json(processedResult);
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
-app.get('/api/ownerCampings', async (req, res) =>{
-    try{
-        const token = req.headers['authorization'].replace('Bearer ','')
+app.get('/api/ownerCampings', async (req, res) => {
+    try {
+        const token = req.headers['authorization'].replace('Bearer ', '')
         const decoded = jwt.verify(token, JWT_SECRET);
         const userIdToken = decoded.userId
 
@@ -274,31 +307,31 @@ app.get('/api/ownerCampings', async (req, res) =>{
             WHERE c.ownerID = ?
             GROUP BY c.ID;`, [userIdToken]
         );
-        const result = {totalEarnings, campingEarnings};
+        const result = { totalEarnings, campingEarnings };
 
         res.json(result);
     }
-    catch (error){
+    catch (error) {
         console.log(error);
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
-app.get('/api/facilities', async(req,res) => {
+app.get('/api/facilities', async (req, res) => {
     const db = new Database();
-    try{
+    try {
         console.log("fetching facilities")
         const result = await db.getQuery('SELECT * FROM facilities ORDER BY ID');
         res.json(result);
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
 //bookings
 app.get('/api/bookingsOverview', async (req, res) => {
     const db = new Database();
-    try{
-        const token = req.headers['authorization'].replace('Bearer ','')
+    try {
+        const token = req.headers['authorization'].replace('Bearer ', '')
         const decoded = jwt.verify(token, JWT_SECRET);
         const userIdToken = decoded.userId
 
@@ -312,8 +345,8 @@ app.get('/api/bookingsOverview', async (req, res) => {
             JOIN bookings b on c.ID = b.campingID
                 JOIN users u ON b.userID = u.ID
             WHERE u.ID = ?
-            ORDER BY b.startDate;`,[userIdToken]
-            
+            ORDER BY b.startDate;`, [userIdToken]
+
         );
         const processedResult = result.map(camping => {
             const formattedStartDate = new Date(camping.startDate).toLocaleDateString('en-GB')
@@ -327,21 +360,21 @@ app.get('/api/bookingsOverview', async (req, res) => {
 
         res.json(processedResult);
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
-app.post('/api/bookings', async(req,res) => {
-    try{
-        const token = req.headers['authorization'].replace('Bearer ','');
+app.post('/api/bookings', async (req, res) => {
+    try {
+        const token = req.headers['authorization'].replace('Bearer ', '');
         const decoded = jwt.verify(token, JWT_SECRET);
         const userIdToken = decoded.userId;
 
-        const {startDate, endDate, campingId, totalPrice} = req.body;
+        const { startDate, endDate, campingId, totalPrice } = req.body;
         // console.log("test1");
 
-        if (totalPrice < 0 || !campingId){
-            return res.status(401).json({ error: 'Foutieve input'});
+        if (totalPrice < 0 || !campingId) {
+            return res.status(401).json({ error: 'Foutieve input' });
         };
         // console.log("test2");
         const db = new Database;
@@ -353,7 +386,7 @@ app.post('/api/bookings', async(req,res) => {
         console.log(`Booking added successfully`)
         res.status(201).send({ message: 'Booking added successfully' });
 
-    }catch (error){
+    } catch (error) {
         res.status(500).send({ error: 'Failed to add booking', details: error })
     }
 
@@ -362,7 +395,7 @@ app.post('/api/bookings', async(req,res) => {
 app.get('/api/bookedDates', async (req, res) => {
     const db = new Database();
     const campingID = req.query.campingID;
-    try{
+    try {
         console.log("fetching bookings for camping " + campingID);
         const result = await db.getQuery(`
             SELECT startDate, endDate 
@@ -374,7 +407,7 @@ app.get('/api/bookedDates', async (req, res) => {
         // console.log(result);
         res.json(result);
     }
-    catch (error){
+    catch (error) {
         res.status(500).send({ error: 'failed to connect to database', details: error.message });
     }
 })
